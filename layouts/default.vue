7cd00c94e8d10aff6af88ce260f0ec8d063964c5
Borys Tyminski <borys@astarium.pl>
2019-12-05 16:40:17 +0100

    add entire issue demo

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <br><br>
+    <v-text-field label="Nazwisko" outlined></v-text-field>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
